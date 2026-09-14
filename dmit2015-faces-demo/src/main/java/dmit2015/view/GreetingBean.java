e607c2bc6757d4557f7c23a69c82217140334da3
Kelly Deng <sweet.kelly.d@gmail.com>
2026-09-13 21:44:02 -0600

    Completed practice activity 4B, added last name to greeting bean

--- dmit2015-faces-demo/src/main/java/dmit2015/view/GreetingBean.java
+++ dmit2015-faces-demo/src/main/java/dmit2015/view/GreetingBean.java
@@ -9,20 +9,30 @@ public class GreetingBean {
 
     private String firstName;
 
+    private String lastName;
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getGreetingMessage() {
-        if (firstName == null || firstName.isBlank()) {
+        if (firstName == null || firstName.isBlank() || lastName == null || lastName.isBlank()) {
             return "";
         }
 
 //        return "Welcome " + firstName + " to DMIT2015!";
-        return String.format("Welcome %s to DMIT2015!", firstName);
+        return String.format("Welcome %s %s to DMIT2015!\n", firstName, lastName);
     }
 }
